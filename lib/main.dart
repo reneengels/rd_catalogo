2d133fa75fa081446095b6173b2771889b25df4f
Rene <reneengels@hotmail.com>
2024-05-05 17:10:09 -0300

    icone, splash screen e background da tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'screens/login.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -21,4 +36,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -7,6 +6,31 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/tela_de_fundo_inicial.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(child: Container()),
+          Container(
+            width: 50,
+            height: 50,
+            child: ElevatedButton(
+              child: const Row(
+                children: [
+                  Text('Comece Agora!'),
+                  Icon(Icons.arrow_forward_rounded),
+                ],
+              ),
+              onPressed: ()=>{},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
