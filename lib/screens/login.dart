d8325fe8717a997d7625d765defa41894d0ac59a
Rene <reneengels@hotmail.com>
2024-05-30 09:51:27 -0300

    icone, splash screen e background da tela inicial (2X)

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,36 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/tela_de_fundo_inicial.jpg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        children: [
-          Expanded(child: Container()),
-          Container(
-            width: 50,
-            height: 50,
-            child: ElevatedButton(
-              child: const Row(
-                children: [
-                  Text('Comece Agora!'),
-                  Icon(Icons.arrow_forward_rounded),
-                ],
-              ),
-              onPressed: ()=>{},
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(50),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/tela_de_fundo_inicial.jpg'),
+              fit: BoxFit.cover,
             ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Color(0xe0000000),
+                Color(0x50000000),
+                Colors.transparent,
+              ],
+              stops: [0.0, 0.6, 1.0],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            padding: const EdgeInsets.all(50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: SizedBox.shrink(),
+                ),
+                Text('Calalogue suas Rosas do Deserto'),
+                Text('A maneira mais fácil de registrar sua coleção'),
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: Container(
+                    width: 150,
+                    height: 60,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('Comece Agora!'),
+                        Icon(Icons.arrow_forward_rounded),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
